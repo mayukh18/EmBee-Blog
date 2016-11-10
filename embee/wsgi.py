b806489cecfcad021a5b37a53860a79037c3264d
mayukh18 <mayukh.superb@gmail.com>
2016-11-10 15:11:02 +0530

    first commit

---
+++ embee/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for embee project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "embee.settings")
+
+application = get_wsgi_application()
